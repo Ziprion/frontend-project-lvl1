4d1f19ff87697d0151126c89a29debaf6ab479fb
Maxim Novikov <maximkanv@gmail.com>
2021-01-23 09:18:50 +0300

    asd

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const greeting = () => {
+  const userName = readlineSync.question("May I have your name? ");
+  return console.log("Hello, " + userName + "!");
+};
