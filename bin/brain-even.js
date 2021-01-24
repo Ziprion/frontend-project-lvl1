4937a97d9090ec56d8a4ea14ad2f62ee46efcd34
Maxim Novikov <maximkanv@gmail.com>
2021-01-24 14:55:39 +0300

    game-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-greeting();
+const userName = greeting();
+console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
+const round = () => {
+    const evenNumber = Math.round(Math.random() * 100) + 1;
+    console.log(`Question: ${evenNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const even = evenNumber % 2 === 0 ? 'yes' : 'no';
+    const positiveReturn = 'Correct!';
+    const negativeReturn = `'${userAnswer}' is wrong answer ;(. Correct answer was '${even}'.\n` + `Let's try again, ${userName}!`
+    const resultRound = userAnswer == even ? 1 : 0;
+    console.log(resultRound === 1 ? positiveReturn : negativeReturn); 
+    return resultRound;
+  };
+const game = (count = 0) => {
+    if (count === 3) {
+        return console.log(`Congratulations, ${userName}!`);
+    }
+    const score = round();
+    if (score === 0) {
+        return;
+    }
+    count += 1;
+    game(count);
+}
+game();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 export default greeting;
